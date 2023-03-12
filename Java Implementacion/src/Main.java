771ef8b36f07b79c916700a25b0547d3acf24a2d
Pawel Harasiuk <pharasiuk01@gmail.com>
2023-03-12 16:59:30 +0100

    Final commit

--- Java Implementacion/src/Main.java
+++ Java Implementacion/src/Main.java
@@ -1,41 +1,62 @@
 public class Main {
-    public static double sin(double x) {
-        // Przybliżenie działa poprawnie tylko w pierwszej ćwiartce
-        x = x % (2*Math.PI);
-
-        // Przybliżenie działa poprawnie tylko w radianach, zamiana stopni na radiany
-        x = Math.toRadians(x);
-
-        // Licznik i mianownik szeregu
-        double numerator = x;
-        double denominator = 1;
-        double result = 0;
-
-        // Obliczanie kolejnych wyrazów szeregu
-        for (int i = 0; i < 10; i++) {
-            if (i % 2 == 0) {
-                result += numerator / denominator;
-            } else {
-                result -= numerator / denominator;
-            }
-
-            numerator *= -x * x;
-            denominator *= (2*i + 2) * (2*i + 3);
+    public static double sin(double rad) {
+        double sin = 0;
+        rad %= (2 * Math.PI);
+        if (rad >= 0 && rad <= (Math.PI) / 2) {
+            sin = obliczSinusSzeregiemTaylora(rad);
+        } else if (rad > (Math.PI) / 2 && rad <= Math.PI) {
+            rad = Math.PI - rad;
+            sin = obliczSinusSzeregiemTaylora(rad);
+        } else if (rad > Math.PI && rad <= Math.PI + (Math.PI) / 2) {
+            rad = -(rad - Math.PI);
+            sin = obliczSinusSzeregiemTaylora(rad);
+        } else if (rad > Math.PI + (Math.PI) / 2 && rad <= 2 * Math.PI) {
+            rad = -(2 * Math.PI - rad);
+            sin = obliczSinusSzeregiemTaylora(rad);
         }
+        return sin;
+    }
 
-        return result;
+    public static double obliczSinusSzeregiemTaylora(double rad) {
+        double przyblizenie = 0;
+        boolean znak = true;
+        for (int i = 1; i < 10 * 2; i += 2) {
+            if (znak) {
+                przyblizenie += potega(rad, i) / silnia(i);
+            } else {
+                przyblizenie -= potega(rad, i) / silnia(i);
+            }
+            znak = !znak;
+        }
+        return przyblizenie;
+    }
+
+    public static int silnia(int x) {
+        if (x <= 0) {
+            return 1;
+        }
+        return silnia(x - 1) * x;
+    }
+
+    public static double potega(double x, int stopien) {
+        double potega = 1;
+        for (int i = 0; i < stopien; i++) {
+            potega *= x;
+        }
+        return potega;
     }
 
     public static void main(String[] args) {
-        // Testowanie funkcji
-        for (int i = 1; i <= 10; i++) {
-            double x = i * Math.PI / 6;
-            double approx = sin(x);
-            double actual = Math.sin(x);
-            double diff = Math.abs(approx - actual);
-            System.out.printf("x = %.3f, sin(x) = %.15f, Math.sin(x) = %.15f, diff = %.15f%n",
-                    x, approx, actual, diff);
+        for (double rad = 0; rad <= 2 * Math.PI; rad += Math.PI / 20) {
+            double sinObliczony = sin(rad);
+            double sinDokladny = Math.sin(rad);
+            double err = Math.abs(sinDokladny - sinObliczony);
+
+            System.out.println("Kat: " + Math.toDegrees(rad) + " stopni");
+            System.out.println("Sinus obliczony: " + sinObliczony);
+            System.out.println("Sinus dokladny: " + sinDokladny);
+            System.out.println("Blad: " + err);
+            System.out.println();
         }
     }
 }
-
